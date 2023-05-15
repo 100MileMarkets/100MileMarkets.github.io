9fb7d8e00470b823a5d7688531af18e7a8cdd539
Steve Johnson <steve@abstractcoder.com>
2023-05-15 13:53:26 -0500

    Added placeholder content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sans": ["Open Sans", ...defaultTheme.fontFamily.sans],
+        "serif": ["Bellota Text", ...defaultTheme.fontFamily.serif],
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
